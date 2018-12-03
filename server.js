b41980a343767daeed8666a86a758e608ec830bb
viacheslav.klepach <viacheslav.klepach@globallogic.com>
2018-12-03 23:10:18 +0300

    Fixed port for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 // const mongoose = require("mongoose");
 const axios = require('axios');
+const port = process.env.PORT || 3000;
 
 // mongoose.Promise = global.Promise;
 // mongoose.connect("mongodb://localhost:27017/node-demo", {useNewUrlParser: true});
